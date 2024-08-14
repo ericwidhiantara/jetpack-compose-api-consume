7f8debf166fc7a9cba6c9b2b7da42bc308546141
eric <nukenin.konoha@gmail.com>
2024-08-14 10:18:30 +0700

    feature: init project

---
+++ shared/src/androidMain/kotlin/com/example/composeapiconsume/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.example.composeapiconsume
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
